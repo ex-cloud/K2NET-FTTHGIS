32b63abe074af346f0744f326817cdc285539802
ex-cloud <andiansyah0425@gmail.com>
2026-02-07 14:48:59 +0700

    update bug

--- target/generated-sources/annotations/com/company/ftthgis/domain/network/mapper/NetworkMapperImpl.java
+++ target/generated-sources/annotations/com/company/ftthgis/domain/network/mapper/NetworkMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-02-07T14:15:26+0700",
+    date = "2026-02-07T14:34:10+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.9 (Eclipse Adoptium)"
 )
 @Component
@@ -114,4 +114,3 @@ public class NetworkMapperImpl implements NetworkMapper {
         return name;
     }
 }
-
